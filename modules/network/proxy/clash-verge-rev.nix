18e97e8ea8a64e7ed5aa1443ec94de8e7a887651
PostCyberPunk <lyhguest2@outlook.com>
2025-09-21 20:41:47 +0800

    fix:new clash verge
    feat:disable dae
    
    d

--- modules/network/proxy/clash-verge-rev.nix
+++ modules/network/proxy/clash-verge-rev.nix
@@ -1,15 +1,31 @@
 {
   lib,
   config,
-  pkgs,
+  mLib,
   ...
 }:
+with mLib;
 with lib; let
-  cfg = config.myConfig.network.proxy;
-in
-  mkIf cfg.clash-verge-rev.enable {
-    environment.systemPackages = with pkgs; [
-      clash-verge-rev
+  cfg = config.myConfig.network.proxy.clash-verge-rev;
+  _ports = config.myConfig.network.proxy.ports;
+in {
+  options.myConfig.network.proxy.clash-verge-rev = {
+    enable = mkBoolOpt false;
+    standalone = mkBoolOpt false;
+    autoStart = mkBoolOpt false;
+  };
+  config = mkIf cfg.enable {
+    programs.clash-verge = mkMerge [
+      {enable = true;}
+      (mkIf cfg.autoStart {
+        autoStart = true;
+      })
+      (mkIf cfg.standalone {
+        tunMode = true;
+        serviceMode = true;
+      })
     ];
-    networking.firewall.allowedTCPPorts = cfg.ports;
-  }
+
+    networking.firewall.allowedTCPPorts = _ports;
+  };
+}
