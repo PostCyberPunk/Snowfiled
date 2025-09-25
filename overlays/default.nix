01ac0d0bfdee38beaf2ba648c74765ed9bd5eae7
PostCyberPunk <lyhguest2@outlook.com>
2025-09-25 18:15:13 +0800

    fix:ulauncher overlay
    avoid libsoup2

--- overlays/default.nix
+++ overlays/default.nix
@@ -22,20 +22,7 @@ inputs: system: [
   #HACK: ulauncher use new webkit to avoid using libsoup2
   (
     self: super: {
-      ulauncher = super.ulauncher.overrideAttrs (old: {
-        buildInputs = (builtins.filter (x: x != super.webkitgtk_4_0) (old.buildInputs or [])) ++ [super.webkitgtk_4_1];
-        nativeBuildInputs = (builtins.filter (x: x != super.webkitgtk_4_0) (old.nativeBuildInputs or [])) ++ [super.fetchpatch];
-        propagatedBuildInputs = builtins.filter (x: x != super.webkitgtk_4_0) (old.propagatedBuildInputs or []);
-        patches =
-          (old.patches or [])
-          ++ [
-            (super.fetchpatch {
-              name = "support-gir1.2-webkit2-4.1.patch";
-              url = "https://src.fedoraproject.org/rpms/ulauncher/raw/rawhide/f/support-gir1.2-webkit2-4.1.patch";
-              hash = "sha256-w1c+Yf6SA3fyMrMn1LXzCXf5yuynRYpofkkUqZUKLS8=";
-            })
-          ];
-      });
+      ulauncher = self.callPackage ./ulauncher/package.nix {};
     }
   )
 ]
